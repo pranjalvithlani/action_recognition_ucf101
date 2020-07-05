50c045aef8b2cc8f86eb1751536bd5c35fc057b9
Pranjal Vithlani <14846666+pranjalvithlani@users.noreply.github.com>
2020-07-05 17:29:43 -0400

    Add files via upload

--- params.py
+++ params.py
@@ -16,11 +16,11 @@ parser.add_argument('data', metavar='DIR', default = './data/frame_data/',
                     help='path to dataset')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
+parser.add_argument('--epochs', default=20, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=256, type=int,
+parser.add_argument('-b', '--batch-size', default=64, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
 parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
                     metavar='LR', help='initial learning rate')
